74318f2e021e61982e4153c360a0854fadd2025d
BladeHicks <97752030+BladeHicks@users.noreply.github.com>
2022-08-28 16:19:37 -0400

    first

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int counter = 1;
+    // int done = 10;
+
+    for (int i = 0; i < 5; i++) {
+        cout << i*i << " " << endl;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << i << "hello world" << endl;
+    }
+
+
+    return 0;
+}
